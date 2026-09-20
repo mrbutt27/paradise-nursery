7c2056ee680eb12a2dcb3506b5443345768b3df8
mrbutt27 <ismailbutt9927@gmail.com>
2026-09-20 16:09:33 +0500

    Add AboutUs component for nursery information

---
+++ src/AboutUs.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './AboutUs.css';
+
+function AboutUs() {
+  return (
+    <div className="about-us-container">
+      <h2>About Paradise Nursery</h2>
+      <p>Welcome to Paradise Nursery, your ultimate destination for bringing nature closer to your home. We specialize in providing healthy houseplants.</p>
+    </div>
+  );
+}
+
+export default AboutUs;
